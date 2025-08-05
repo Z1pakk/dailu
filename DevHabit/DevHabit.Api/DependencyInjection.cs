c61c6290512e8c8d101ac9beb45f202711b5010d
vlad <wladik420@gmail.com>
2025-08-05 22:27:08 +0300

    Refactor and cleanup Program.cs

---
+++ DevHabit/DevHabit.Api/DependencyInjection.cs
@@ -0,0 +1,120 @@
+using DevHabit.Api.Database;
+using DevHabit.Api.DTOs.Habits;
+using DevHabit.Api.Entities;
+using DevHabit.Api.Middleware;
+using DevHabit.Api.Services;
+using DevHabit.Api.Services.Sorting;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Newtonsoft.Json.Serialization;
+using Npgsql;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace DevHabit.Api;
+
+public static class DependencyInjection
+{
+    public static WebApplicationBuilder AddControllers(this WebApplicationBuilder builder)
+    {
+        builder
+            .Services.AddControllers(options =>
+            {
+                // Configure the API to return 406 Not Acceptable if the requested format(Accept header) is not supported
+                options.ReturnHttpNotAcceptable = true;
+            })
+            .AddNewtonsoftJson(options =>
+                options.SerializerSettings.ContractResolver =
+                    new CamelCasePropertyNamesContractResolver()
+            )
+            .AddXmlSerializerFormatters();
+
+        builder.Services.AddOpenApi();
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddErrorHandling(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddProblemDetails(options =>
+        {
+            options.CustomizeProblemDetails = context =>
+            {
+                context.ProblemDetails.Extensions.TryAdd(
+                    "requestId",
+                    context.HttpContext.TraceIdentifier
+                );
+            };
+        });
+
+        builder.Services.AddExceptionHandler<ValidationExceptionHandler>();
+        builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddDatabase(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options
+                .UseNpgsql(
+                    builder.Configuration.GetConnectionString("Database"),
+                    npgsqlOptions =>
+                        npgsqlOptions.MigrationsHistoryTable(
+                            HistoryRepository.DefaultTableName,
+                            Schemas.Application
+                        )
+                )
+                .UseSnakeCaseNamingConvention()
+        );
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddObservability(this WebApplicationBuilder builder)
+    {
+        builder
+            .Services.AddOpenTelemetry()
+            .ConfigureResource(resource =>
+            {
+                resource.AddService(builder.Environment.ApplicationName);
+            })
+            .WithTracing(tracing =>
+            {
+                tracing.AddAspNetCoreInstrumentation().AddHttpClientInstrumentation().AddNpgsql();
+            })
+            .WithMetrics(metrics =>
+            {
+                metrics
+                    .AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddRuntimeInstrumentation();
+            })
+            .UseOtlpExporter();
+
+        builder.Logging.AddOpenTelemetry(options =>
+        {
+            options.IncludeFormattedMessage = true;
+            options.IncludeScopes = true;
+        });
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddApplicationServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddValidatorsFromAssemblyContaining<Program>(includeInternalTypes: true);
+
+        builder.Services.AddTransient<SortMappingProvider>();
+        builder.Services.AddSingleton<
+            ISortMappingDefinition,
+            SortMappingDefinition<HabitDto, Habit>
+        >(_ => HabitMappings.SortMapping);
+        builder.Services.AddTransient<DataShapingService>();
+
+        return builder;
+    }
+}
--- DevHabit/DevHabit.Api/Program.cs
+++ DevHabit/DevHabit.Api/Program.cs
@@ -1,90 +1,14 @@
-using DevHabit.Api.Database;
-using DevHabit.Api.DTOs.Habits;
-using DevHabit.Api.Entities;
+using DevHabit.Api;
 using DevHabit.Api.Extensions;
-using DevHabit.Api.Middleware;
-using DevHabit.Api.Services;
-using DevHabit.Api.Services.Sorting;
-using FluentValidation;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Migrations;
-using Newtonsoft.Json.Serialization;
-using Npgsql;
-using OpenTelemetry;
-using OpenTelemetry.Metrics;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder
-    .Services.AddControllers(options =>
-    {
-        // Configure the API to return 406 Not Acceptable if the requested format(Accept header) is not supported
-        options.ReturnHttpNotAcceptable = true;
-    })
-    .AddNewtonsoftJson(options =>
-        options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver()
-    )
-    .AddXmlSerializerFormatters();
-
-builder.Services.AddValidatorsFromAssemblyContaining<Program>(includeInternalTypes: true);
-builder.Services.AddProblemDetails(options =>
-{
-    options.CustomizeProblemDetails = context =>
-    {
-        context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
-    };
-});
-
-builder.Services.AddExceptionHandler<ValidationExceptionHandler>();
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-
-builder.Services.AddOpenApi();
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options
-        .UseNpgsql(
-            builder.Configuration.GetConnectionString("Database"),
-            npgsqlOptions =>
-                npgsqlOptions.MigrationsHistoryTable(
-                    HistoryRepository.DefaultTableName,
-                    Schemas.Application
-                )
-        )
-        .UseSnakeCaseNamingConvention()
-);
-
-builder
-    .Services.AddOpenTelemetry()
-    .ConfigureResource(resource =>
-    {
-        resource.AddService(builder.Environment.ApplicationName);
-    })
-    .WithTracing(tracing =>
-    {
-        tracing.AddAspNetCoreInstrumentation().AddHttpClientInstrumentation().AddNpgsql();
-    })
-    .WithMetrics(metrics =>
-    {
-        metrics
-            .AddAspNetCoreInstrumentation()
-            .AddHttpClientInstrumentation()
-            .AddRuntimeInstrumentation();
-    })
-    .UseOtlpExporter();
-
-builder.Logging.AddOpenTelemetry(options =>
-{
-    options.IncludeFormattedMessage = true;
-    options.IncludeScopes = true;
-});
-
-builder.Services.AddTransient<SortMappingProvider>();
-builder.Services.AddSingleton<ISortMappingDefinition, SortMappingDefinition<HabitDto, Habit>>(_ =>
-    HabitMappings.SortMapping
-);
-builder.Services.AddTransient<DataShapingService>();
+    .AddControllers()
+    .AddErrorHandling()
+    .AddDatabase()
+    .AddObservability()
+    .AddApplicationServices();
 
 WebApplication app = builder.Build();
 
